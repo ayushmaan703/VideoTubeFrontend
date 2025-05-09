8f9a836dad2106df0f18552726f376c3fd6dcb97
Ayushmaan <ayushmaankumar5@gmail.com>
2025-05-10 01:10:02 +0530

    refactor: comment out server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "https://backend-learning-diiq.onrender.com/api/v1",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  // server: {
+  //   port: 5173,
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://backend-learning-diiq.onrender.com/api/v1",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
 });
